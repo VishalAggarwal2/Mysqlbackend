4c47a9c0b7f490beb120e15a388159b14f17adac
VishalAggarwal2 <136411362+VishalAggarwal2@users.noreply.github.com>
2023-09-25 19:50:40 +0530

    second  commit

--- server/index.js
+++ server/index.js
@@ -13,8 +13,8 @@ const db = mysql.createConnection({
         database:"books"
 })
 const app = express();
-app.use(cors());
-app.use(express.json()); 
+
+app.use(express.json());
 app.get("/",(req,res)=>{ 
 res.json("Hello Its A Backend")
 })
